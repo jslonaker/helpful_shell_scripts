096e68cd6dcfba6aa7d2a465e176ca3ac2f488e7
jslonaker <40641052+jslonaker@users.noreply.github.com>
2018-10-28 02:49:02 -0400

    Update webmin_install.sh

--- webmin_install.sh
+++ webmin_install.sh
@@ -1,4 +1,4 @@
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.831_all.deb &&
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.890_all.deb &&
 apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python -y &&
-dpkg --install webmin_1.831_all.deb &&
-rm webmin_1.831_all.deb
+dpkg --install webmin_1.890_all.deb &&
+
